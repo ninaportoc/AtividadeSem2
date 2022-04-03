3198f4293e308ea9a2e94c11298f97fc4e61eb2e
Marina Porto <marinaporto334@gmail.com>
2022-04-03 10:15:26 -0300

    Eu encontrei o valor de uma mercadoria de 500 reais com 20% de desconto

---
+++ 1mercadoria.js
@@ -0,0 +1,8 @@
+let PrecoMercadoria = 500 
+console.log(PrecoMercadoria)
+
+let VintePorCentodaMercadoria = 20*500/100
+console.log(VintePorCentodaMercadoria)
+
+let ValordaMercadoriaDesconto = PrecoMercadoria - VintePorCentodaMercadoria
+console.log(ValordaMercadoriaDesconto)
